dbe8e73a52457631bcc36aa896d3de64aa5fe444
conor <cyuen10@gmail.com>
2023-03-13 08:48:08 -0400

    New ThreeJS Project: scaffolding, added grand piano model, added post-processing fx

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useMemo } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Sky, Environment, OrbitControls } from '@react-three/drei'
+import { Model } from './components/Grandpiano'
+
+import * as THREE from 'three'
+import { EffectComposer, Noise, Bloom, Vignette, DepthOfField, Outline } from '@react-three/postprocessing'
+
+function App() {
+
+  const materials = useMemo(() => {
+    return {
+      pianoMaterial: new THREE.MeshToonMaterial({
+        color: new THREE.Color('#dfdfdf')
+      }),
+      blackKeyMaterial: new THREE.MeshToonMaterial({
+        color: new THREE.Color('black')
+      }),
+      whiteKeyMaterial: new THREE.MeshLambertMaterial({
+        color: new THREE.Color('#ffffff')
+      })
+    }
+  })
+
+
+
+  return (
+    <Canvas shadows shadowMap>
+      <OrbitControls target={[0, 1, 0]} />
+      <color attach="background" args={["#FFDFD3"]} />
+      {/* <Environment preset='sunset' /> */}
+      <pointLight position={[0,5,4]} castShadow intensity={.5}/>
+      <ambientLight intensity={.6} />
+      <Model castshadow materials={materials} />
+      <mesh position={[0, .62, 0]} rotation={[-Math.PI / 2   , 0, 0]} receiveShadow>
+        <planeGeometry args={[10, 10, 10]} />
+        <shadowMaterial color='#FFDFD3' />
+      </mesh>
+      <EffectComposer>
+        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
+        <Bloom luminanceThreshold={.75} luminanceSmoothing={0.1} height={300} />
+        <Noise opacity={0.20} />
+        {/* <Vignette eskil={false} offset={0.01} darkness={1.1} /> */}
+        {/* <Outline blur edgeStrength={100} /> */}
+      </EffectComposer>
+    </Canvas>
+  )
+}
+
+export default App
