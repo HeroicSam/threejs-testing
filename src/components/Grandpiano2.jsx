dc22d091212231fd8ef54aa54dab8fd79d48dd9a
HeroicSam <cyuen10@gmail.com>
2023-03-18 03:23:21 -0400

    fix: changed the <Html /> component from vanilla div to an iframe to fix transform issue

--- src/components/Grandpiano2.jsx
+++ src/components/Grandpiano2.jsx
@@ -6,38 +6,36 @@ export function Model({ materials, toTablet }) {
   const { nodes } = useGLTF('/grandpiano2.glb')
   return (
     <>
-      <Tablet toTablet={toTablet} />
-      <group dispose={null}>
-        <mesh castShadow geometry={nodes.Main.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey001.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey002.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey003.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey004.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey005.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey006.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.blackkey007.geometry} material={materials.blackKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys01.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys002.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys003.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys004.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys005.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys006.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys007.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.White_Keys008.geometry} material={materials.whiteKeyMaterial} />
-        <mesh castShadow geometry={nodes.FrontBrassPiece.geometry} material={materials.brassMaterial} />
-        <mesh castShadow geometry={nodes.BackBrassPiece.geometry} material={materials.brassMaterial} />
-        <mesh castShadow geometry={nodes.BrassBridges.geometry} material={materials.brassMaterial} />
-        <mesh castShadow geometry={nodes.Wires01.geometry} material={nodes.Wires01.material} />
-        <mesh castShadow geometry={nodes.Wires02.geometry} material={nodes.Wires02.material} />
-        <mesh castShadow geometry={nodes.Pegs.geometry} material={materials.redMaterial} />
-        <mesh castShadow geometry={nodes.RedBar.geometry} material={materials.redMaterial} />
-        <mesh castShadow geometry={nodes.WoodBase.geometry} material={materials.woodMaterial} />
-        <mesh castShadow geometry={nodes.Pegs02.geometry} material={materials.redMaterial} />
-        <mesh castShadow geometry={nodes.Wires03.geometry} material={nodes.Wires03.material} />
-        <mesh castShadow geometry={nodes.Tablet.geometry} material={materials.blackKeyMaterial} />
-        <mesh geometry={nodes.Logo.geometry} material={materials['Material.001']} position={[-0.05, 0.93, 1]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
-      </group>
+      <Tablet />
+      <mesh castShadow geometry={nodes.Main.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey001.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey002.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey003.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey004.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey005.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey006.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.blackkey007.geometry} material={materials.blackKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys01.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys002.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys003.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys004.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys005.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys006.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys007.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.White_Keys008.geometry} material={materials.whiteKeyMaterial} />
+      <mesh castShadow geometry={nodes.FrontBrassPiece.geometry} material={materials.brassMaterial} />
+      <mesh castShadow geometry={nodes.BackBrassPiece.geometry} material={materials.brassMaterial} />
+      <mesh castShadow geometry={nodes.BrassBridges.geometry} material={materials.brassMaterial} />
+      <mesh castShadow geometry={nodes.Wires01.geometry} material={nodes.Wires01.material} />
+      <mesh castShadow geometry={nodes.Wires02.geometry} material={nodes.Wires02.material} />
+      <mesh castShadow geometry={nodes.Pegs.geometry} material={materials.redMaterial} />
+      <mesh castShadow geometry={nodes.RedBar.geometry} material={materials.redMaterial} />
+      <mesh castShadow geometry={nodes.WoodBase.geometry} material={materials.woodMaterial} />
+      <mesh castShadow geometry={nodes.Pegs02.geometry} material={materials.redMaterial} />
+      <mesh castShadow geometry={nodes.Wires03.geometry} material={nodes.Wires03.material} />
+      <mesh castShadow geometry={nodes.Tablet.geometry} material={materials.blackKeyMaterial} />
+      <mesh geometry={nodes.Logo.geometry} material={materials['Material.001']} position={[-0.05, 0.93, 1]} rotation={[Math.PI / 2, 0, 0]} scale={0.04} />
     </>
 
   )
--- src/components/Tablet.jsx
+++ src/components/Tablet.jsx
@@ -4,16 +4,14 @@ export default function Tablet({ toTablet }){
 
     return (
         <Html
-          position={[0.0765, 1.194, .835]}
+          position={[0.1095, 1.212, .828]}
           rotation-x={-Math.PI / 9}
           distanceFactor={.056}
           wrapperClass="tablet"
           occlude
           transform
         >
-          {/* <div>
-            <h1>BOOBEAR</h1>
-          </div> */}
+          <iframe src="../../index.html" />
         </Html>
     )
 }
